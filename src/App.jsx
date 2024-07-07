a27e65054bde0fc06a06b8758cc009ff5366e10e
Aditya <aditya.diwakar2003@gmail.com>
2024-07-07 11:20:52 +0530

    Textify app beta

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import Aboutus from "./aboutus";
+import Alert from "./Alert";
+import Navbar from "./Navbar";
+import Textform from "./Textform";
+import Textarea from "./Textform"
+import React,{ useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+
+  const[mode,setMode] =useState('light');
+  const[alert,setAlert] =useState(null);
+
+  const showAlert=(message,type)=>{
+    setAlert({
+      msg:message,
+      type:type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+
+    const toggleMode = () => {
+      if(mode==='light'){
+        setMode('dark');
+        document.body.style.backgroundColor='#042743';
+        showAlert("Dark mode has been enabled","success");
+
+      }else{
+        setMode('light');
+        document.body.style.backgroundColor = 'white';
+        showAlert("Light mode has been enabled","success");
+      }
+    }
+
+
+    
+  return(
+    <>
+      <Router>
+    <Navbar title="Textify" mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert}/>
+    <div className="container my-3">
+        <Routes>
+          {/* /users --> Component 1
+            /users/home --> Component 2 */}
+          <Route path="/about" element={<Aboutus mode={mode}/>} />
+          <Route path="/" element={<Textform mode={mode} showAlert={showAlert} />} />
+        </Routes>
+      </div>
+      {/* <Textform mode={mode} showAlert={showAlert}/>
+      <Aboutus/> */}
+    </Router>
+    </>
+  ); 
+}
+
+export default App
---
+++ src/Textform.jsx
@@ -0,0 +1,67 @@
+import React ,{ useState } from "react";
+
+function Textform(props){
+    
+    const[text,setText] = useState('');
+
+    const handleuppercase = () => {
+        const newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert('Converted to uppercase!', 'success');
+      };
+ 
+    const handlelowercase =() => {
+        let lower= text.toLowerCase();
+        setText(lower);
+        props.showAlert("Converted to lowercase!", "success");
+    };
+    
+    const handleOnChange = (event)=>{
+        setText(event.target.value);
+    }
+
+    const handleclear = () => {
+        let newtext = '';
+        setText(newtext);
+        props.showAlert("Text cleared","warning")
+    }
+
+    const handlecopy = () =>{
+        navigator.clipboard.writeText(text); 
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
+
+  
+   
+    return(
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
+            <div className="mb-3">
+                <h1>Enter the text below:</h1>
+                <label htmlFor="textarealabel" className="label"></label>
+                <textarea className="form-control" id="textarea" value={text} rows="8" onChange={handleOnChange} style={{backgroundColor:props.mode ==='dark' ? '#13466e':'white',color:props.mode==="dark"?"white" : '#042743'}}></textarea><br />
+                <button id="touppercasebtn" onClick={handleuppercase}> Convert to Upper Case</button>
+                <button id="touppercasebtn" onClick={handlelowercase}> Convert to Lower Case</button>
+                <button id="clearbtn" onClick={handleclear}>Clear Text</button>
+                <button id="copybtn" onClick={handlecopy}>Copy Text</button>
+                <button id="speak" onClick={speak} >Speak</button>
+           
+
+</div>
+        </div>
+             <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+                   <h2>Your Text Summary </h2>
+                   <p> {text.split(" ").filter((element)=> {return element.length !==0;}).length} words  and {text.length} characters.</p>
+                   <h2>Preview</h2>
+                   <p>{text}</p>
+                </div>
+        </>
+    );
+}
+export default Textform
